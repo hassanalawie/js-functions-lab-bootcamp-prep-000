97b61e1f0e4c89bb4d786b9434b7fa627c7723b1
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-14 00:06:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // write your code below!
+var name="you"
 function happyHolidays(){
   
   return "Happy holidays!"
@@ -12,12 +13,12 @@ function happyHolidaysTo(name){
 
 function happyHolidayTo(holiday, name){
   var holiday= "Independance Day"
-  
   return 'Happy ${holiday}, ${name}!'
 }
 
 function holidayCountdown (days, holiday){
-  
+  var holiday = "Mother\'s day"
+  var days= 20
   return 'It\'s ${days} days until ${holiday}!' 
   
 }
